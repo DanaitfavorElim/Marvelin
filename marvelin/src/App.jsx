7cbed0d778b44fa10eb3396658d316895842b43e
DanaitfavorElim <danaitb.daniel@gmail.com>
2025-10-19 15:59:59 +0400

    Update App.jsx

--- marvelin/src/App.jsx
+++ marvelin/src/App.jsx
@@ -426,7 +426,7 @@ function App() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 Marval In Company Limited. All rights reserved.</p>
+            <p>&copy; 2024 Marvel In Company Limited. All rights reserved.</p>
           </div>
         </div>
       </footer>
